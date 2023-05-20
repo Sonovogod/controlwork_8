292a897af5bdbb9eefc93fce8685c8383b4193ee
Nikita <nikita.dronoff@gmail.com>
2023-05-20 11:56:30 +0600

    init

---
+++ Forum/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ChatContext>(options => options.UseNpgsql(connection))
+    .AddIdentity<User, IdentityRole>(options =>
+    {
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 5;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+    })
+    .AddEntityFrameworkStores<ChatContext>();
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IFileService, FileService>();
+
+var app = builder.Build();
+var serviceProvider = app.Services;
+
+using var scope = serviceProvider.CreateScope();
+try
+{
+    UserManager<User> userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+    RoleManager<IdentityRole> roleManager =  scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    await AdminInitializer.SeedAdminUser(roleManager, userManager);
+}
+catch (Exception e)
+{
+    var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+    logger.LogCritical(e, "Не удалось создать админа в БД");
+}
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
